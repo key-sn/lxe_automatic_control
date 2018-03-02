994d16b1be4372e11510252b8d3178747ab95550
kei ishihara <k.passtd@gmail.com>
2018-03-03 00:39:17 +0900

    readmeの更新

--- lxe_automatic_control.py
+++ lxe_automatic_control.py
@@ -1,11 +1,4 @@
 # coding:utf-8
-##############################
-#初期設定
-#ヒーターの電圧は30V、初期目標温度は-90
-# $ sudo nohup python lxe_automatic_control.py & 起動→「+suspend」で動かない場合は「$ fg」とうてば動くかと
-# $ ps aux | grep lxe_automatic_control プロセスが動いているかどうか確認
-# $ sudo kill プロセス番号  プロセスを終了する場合
-##############################
 import serial # シリアル通信用
 import lxml # scraping用
 import requests # scraping用
